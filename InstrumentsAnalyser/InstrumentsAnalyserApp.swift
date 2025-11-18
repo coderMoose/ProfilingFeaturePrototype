3ef6e8dadec9047bd4853496ea56bfee2e2c5131
Daniel <daniel_francis14@icloud.com>
2025-11-18 14:11:37 -0500

    Initial Commit

---
+++ InstrumentsAnalyser/InstrumentsAnalyserApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstrumentsAnalyserApp.swift
+//  InstrumentsAnalyser
+//
+//  Created by Daniel on 2025-11-18.
+//
+
+import SwiftUI
+
+@main
+struct InstrumentsAnalyserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
